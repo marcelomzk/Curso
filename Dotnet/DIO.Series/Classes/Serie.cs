97ee5acc8852e0f5b4d3f4eaf9bf71992ab6c055
marcelomzk <marcelo.mzk@gmail.com>
2021-09-18 21:12:18 -0300

    Criando EntidadeBase e classes Serie e Genero

---
+++ Dotnet/DIO.Series/Classes/Serie.cs
@@ -0,0 +1,45 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        // Metodos
+
+        public Serie(int Id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            //Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.enviroment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + enviroment.NewLine;
+            retorno += "Titulo: " + this.Titulo + enviroment.NewLine;
+            retorno += "Descricao: " + this.Descricao + enviroment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + enviroment.NewLine;
+
+            return retorno;
+            
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+    }
+}
---
+++ Dotnet/DIO.Series/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //classe abstrata nao pode ser instanciada
+            //EntidadeBase minhaClasse = new EntidadeBase();
+
+            Serie meuObjeto = new Serie();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
