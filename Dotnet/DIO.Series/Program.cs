dfc41206a31caa9f4154f74db23cde693600f744
marcelomzk <marcelo.mzk@gmail.com>
2021-09-20 17:50:12 -0300

    Implementar método ObterOpcaoUsuario()

--- Dotnet/DIO.Series/Program.cs
+++ Dotnet/DIO.Series/Program.cs
@@ -13,5 +13,23 @@ namespace DIO.Series
 
             Console.WriteLine("Hello World!");
         }
+
+        private string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().toUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
     }
 }
